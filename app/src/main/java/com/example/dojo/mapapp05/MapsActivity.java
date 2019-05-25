13c771462f8fa30bc395633e5e593ed88b89a9a2
Konsho0121 <trt.kon@icloud.com>
2019-05-25 17:36:03 +0900

    Third Commit

--- app/src/main/java/com/example/dojo/mapapp05/MapsActivity.java
+++ app/src/main/java/com/example/dojo/mapapp05/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.dojo.mapapp05;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,8 +17,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
+    private LocationManager LocationManager;
     private GoogleMap mMap;
 
     @Override
@@ -22,6 +30,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        LocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new  String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },1);
+            return;
+        }
+        LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+    }
+
+   @Override
+    public void onStatusChanged(String provider, int status, Bundle extras){
+
+    }
+
+     @Override
+     public void onLocationChanged(Location location){
+         mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
+     }
+
+    @Override
+    public void onProviderEnabled(String provider){
+
+    }
+    @Override
+    public void onProviderDisabled(String provider){
+
     }
 
 
